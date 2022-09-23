7e94858d31eb318ad053e2cc373d48fd767da72a
Qudus <digitalgeek13@gmail.com>
2022-09-23 10:51:57 +0100

    body parser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app= express();
+const bodyParser = require ('body-parser');
 const port = 5000
 
+app.use(bodyParser.json());
 app.listen(port, ()=>{
     console.log(`server started on port ${port}`)
 })
@@ -15,4 +17,22 @@ app.get(`/qudus`, (req, res) => {
 
 app.get(`/transaction/payment`, (req, res) => {
     res.send (`what transaction do you want to perform?`)
+})
+
+app.post(`/create`, (req, res) => {
+    const data = req.body
+    if(data.firstname == "" || data.lastname =="" || data.email == ""){
+        res.send({ 
+            message: "All fields are required"
+        })
+    }else{
+        res.send({
+            message: `User information accepted, Welcome user ${data.firstname}, ${data.lastname}`
+        })
+    }
+})
+
+app.get(`/today`, (req, res) =>{
+    let today =new Date()
+    res.send(today)
 })
